f33e5d04fb625a45c53269bba5882b20be4ab64b
Russell Smith <ukd1@users.noreply.github.com>
2022-10-20 21:08:30 -0700

    First try at fixing Que. Moved to AR, as it doesn't support direct connection, and wants a pool otherwise

--- queues/que.rb
+++ queues/que.rb
@@ -1,11 +1,15 @@
-$pg.async_exec "DROP TABLE IF EXISTS que_jobs"
+$pg.async_exec "DROP TABLE IF EXISTS que_jobs, que_values, que_lockers CASCADE"
+$pg.async_exec "DROP FUNCTION IF EXISTS que_validate_tags, que_job_notify, que_state_notify"
 
-Que.connection = $pg
-Que.migrate!
+ActiveRecord::Base.establish_connection(DATABASE_URL)
+ActiveRecord::Base.connection.raw_connection.async_exec "SET SESSION synchronous_commit = #{SYNCHRONOUS_COMMIT}"
+
+Que.connection = ActiveRecord
+Que.migrate!(version: 7)
 
 $pg.async_exec <<-SQL
-  INSERT INTO que_jobs (job_class, priority)
-  SELECT 'QuePerpetualJob', 1
+  INSERT INTO que_jobs (job_class, priority, job_schema_version)
+  SELECT 'QuePerpetualJob', 1, 1
   FROM generate_Series(1,#{JOB_COUNT}) AS i;
 SQL
 
